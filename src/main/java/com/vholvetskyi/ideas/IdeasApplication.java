0d2aa40773bec00637f54dee809dc0a9fd76f5be
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2023-07-15 17:47:56 +0200

    Create IdeasApplication class

---
+++ src/main/java/com/vholvetskyi/ideas/IdeasApplication.java
@@ -0,0 +1,11 @@
+package com.vholvetskyi.ideas;
+
+public class IdeasApplication {
+    public static void main(String[] args) {
+        new IdeasApplication().start();
+    }
+
+    private void start() {
+        System.out.println("Start app..");
+    }
+}
