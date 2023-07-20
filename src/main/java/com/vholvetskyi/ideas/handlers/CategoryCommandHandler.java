3d2c1f68ab8689edac84e2a5b73b0d9340fc145e
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2023-07-20 17:07:31 +0200

    Create CategoryCommandHandler

--- src/main/java/com/vholvetskyi/ideas/handlers/CategoryCommandHandler.java
+++ src/main/java/com/vholvetskyi/ideas/handlers/CategoryCommandHandler.java
@@ -1,2 +1,47 @@
-package com.vholvetskyi.ideas.handlers;public class CategoryCommandHandler {
+package com.vholvetskyi.ideas.handlers;
+
+import com.vholvetskyi.ideas.dao.CategoryDao;
+import com.vholvetskyi.ideas.input.UserInputCommand;
+import com.vholvetskyi.ideas.model.Category;
+
+import java.util.List;
+
+public class CategoryCommandHandler extends BaseCommandHandler {
+
+    private static final String COMMAND_NAME = "category";
+
+    private CategoryDao categoryDao;
+
+    public CategoryCommandHandler() {
+        this.categoryDao = new CategoryDao();
+    }
+
+    @Override
+    protected String getCommandName() {
+        return COMMAND_NAME;
+    }
+
+    @Override
+    public void handle(UserInputCommand command) {
+        switch (command.getAction()) {
+            case "list":
+                System.out.println("List of categories...");
+                List<Category> categories = categoryDao.findAll();
+                categories.forEach(System.out::println);
+                break;
+
+            case "add":
+                System.out.println("Add category");
+                String categoryName = command.getParam().get(0);
+                categoryDao.add(new Category(categoryName));
+                break;
+
+            default: {
+                throw new IllegalArgumentException(
+                        String.format("Unknown action: %s from command: %s",
+                                command.getAction(), command.getCommand()));
+            }
+        }
+
+    }
 }
